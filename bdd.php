fe2db3ab6e429532139b79fdda47b22a559e277f
Pierre-Yves <pierreyves.augem@gmail.com>
2020-11-17 09:44:00 +0100

    Update bdd.php

--- bdd.php
+++ bdd.php
@@ -1,7 +1,7 @@
 <?php
 
 try{
-	$pdo = new PDO('mysql:host=localhost;port=3306;dbname=ppe4-2;', 'root', 'apmq10',array(
+	$bdd = new PDO('mysql:host=localhost;port=3306;dbname=ppe4-2;', 'root', 'apmq10',array(
         PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8',
         PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
     ));
